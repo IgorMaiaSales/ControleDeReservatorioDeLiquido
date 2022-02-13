de526665e16ca9faea0b55e41102d91c91d70184
Rodrigo <rodrigohiury@alu.ufc.br>
2022-02-13 20:30:15 -0300

    Exclusão arquivo lcd.c não utilizado

--- Core/lcd.c
+++
@@ -1,8 +0,0 @@
-/*
- *	@file	lcd.c
- *	@brief	Control functions for LCD panel
- *	@author Rodrigo Hiury
- *	Project created for Microprocessed Systems' class of Federal University of Ceará
- *
- */
-
